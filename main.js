75e739a64342c1b52a3691da5c45328b1e3e00fa
Joseph Sweet <Joeysweet0341@gmail.com>
2024-08-27 05:24:47 -0400

    added basic createBoard function

---
+++ main.js
@@ -0,0 +1,13 @@
+createBoard(16);
+
+function createBoard(size) {
+
+    let board = document.querySelector(".board");
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const cell = document.createElement('div');
+        board.appendChild(cell);
+    }
+}
