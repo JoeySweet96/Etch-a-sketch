162ee2de74612e0ef0df7de010f68c22f0c15b8d
Joseph Sweet <Joeysweet0341@gmail.com>
2024-08-27 06:44:40 -0400

    added basic functionality

--- main.js
+++ main.js
@@ -1,13 +1,42 @@
-createBoard(16);
+
+let popButton = document.querySelector("#popup");
+popButton.addEventListener('click', function () {
+    let size = getSize();
+    createBoard(size);
+});
 
 function createBoard(size) {
 
     let board = document.querySelector(".board");
+
+    board.innerHTML = '';
+
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    for (let i = 0; i < size * size; i++) {
-        const cell = document.createElement('div');
-        board.appendChild(cell);
+    let area = size * size;
+
+    for (let i = 0; i < area; i++) {
+        let div = document.createElement('div');
+        div.addEventListener('mouseover', function () {
+            this.style.backgroundColor = 'yellow';
+        });
+        board.insertAdjacentElement("beforeend", div);
+    }
+}
+
+function getSize() {
+    let input = prompt("Choose a size: ");
+    let message = document.querySelector("#message");
+
+    if (input == "") {
+        message.innerHTML = "Please pick a size";
+    }
+    else if (input <= 0 || input >= 100) {
+        message.innerHTML = "invalid number";
+    }
+    else {
+        message.innerHTML = input;
+        return input;
     }
 }
